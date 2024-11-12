316d5fd92cef4b150de9c26804a4858ecc04a5d2
Nikita Gordeev <ngordeev@neoflex.ru>
2024-11-12 20:37:41 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+const placeElement = cardTemplate.querySelector(".places__item");
 
-// @todo: DOM узлы
+function makeCard(cardData, deleteButtonCallback) {
+    const placeElementClone = placeElement.cloneNode(true);
+    placeElementClone.querySelector(".card__title").textContent = cardData.name;
+    placeElementClone.querySelector(".card__image").src = cardData.link;
+    placeElementClone.querySelector(".card__image").alt = "Пейзажное фото места " + cardData.name;
+    placeElementClone
+        .querySelector(".card__delete-button")
+        .addEventListener("click", deleteButtonCallback);
+    return placeElementClone;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(ev) {
+    ev.target.closest(".places__item").remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((item) => placesList.append(makeCard(item, deleteCard)));
