eb23da9602c8e5256b2b3935382c60fbab2dd27b
Nikita Gordeev <ngordeev@neoflex.ru>
2024-11-12 21:33:05 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,15 @@ const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 const placeElement = cardTemplate.querySelector(".places__item");
 
-function makeCard(cardData, deleteButtonCallback) {
+function makeCard(cardData) {
     const placeElementClone = placeElement.cloneNode(true);
     placeElementClone.querySelector(".card__title").textContent = cardData.name;
-    placeElementClone.querySelector(".card__image").src = cardData.link;
-    placeElementClone.querySelector(".card__image").alt = "Пейзажное фото места " + cardData.name;
+    const cardImage = placeElementClone.querySelector(".card__image");
+    cardImage.src = cardData.link;
+    cardImage.alt = "Пейзажное фото места " + cardData.name;
     placeElementClone
         .querySelector(".card__delete-button")
-        .addEventListener("click", deleteButtonCallback);
+        .addEventListener("click", deleteCard);
     return placeElementClone;
 }
 
@@ -17,4 +18,4 @@ function deleteCard(ev) {
     ev.target.closest(".places__item").remove();
 }
 
-initialCards.forEach((item) => placesList.append(makeCard(item, deleteCard)));
+initialCards.forEach((item) => placesList.append(makeCard(item)));
